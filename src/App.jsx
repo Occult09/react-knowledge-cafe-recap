351d3d84f84e4a7decf228d9efb0d4b3f5037240
Occult09 <maheermusfiq221@gmail.com>
2025-12-15 14:39:56 +0600

    set basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
 
   return (
-    <>
+    <main className='mx-auto container'>
       <Header></Header>
-      <Blogs></Blogs>
-    </>
+      <div className='md:flex mx-5 p-4'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+    </main>
   )
 }
 
